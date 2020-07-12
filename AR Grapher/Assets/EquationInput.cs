d3045acd20ec836b79800ebce5b3643f97e45ea1
Nick Liu <nickliu02@gmail.com>
2020-07-11 20:46:42 -0400

    Update EquationInput.cs

--- AR Grapher/Assets/EquationInput.cs
+++ AR Grapher/Assets/EquationInput.cs
@@ -1,16 +1,16 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class EquationInput : MonoBehaviour {
     public string equationString;
-    public string boundMinX;
-    public string boundMaxX;
-    public string boundMinY;
-    public string boundMaxY;
+    public int boundMinX;
+    public int boundMaxX;
+    public int boundMinY;
+    public int boundMaxY;
 
-    public double answer;
 
     public GameObject inputField;
     public GameObject minXField;
@@ -18,18 +18,62 @@ public class EquationInput : MonoBehaviour {
     public GameObject minYField;
     public GameObject maxYField;
 
+    private const int totalPoints = 200;
+    double[,,] answer = new double[totalPoints, totalPoints, 3];
+
     public void GetInputs() {
         equationString = inputField.GetComponent<Text>().text;
-        boundMinX = minXField.GetComponent<Text>().text;
-        boundMaxX = maxXField.GetComponent<Text>().text;
-        boundMinY = minYField.GetComponent<Text>().text;
-        boundMaxY = maxYField.GetComponent<Text>().text;
-        MathParser mathParser = new MathParser();
-        answer = mathParser.Parse(equationString, true);
+        boundMinX = Convert.ToInt32(minXField.GetComponent<Text>().text);
+        boundMaxX = Convert.ToInt32(maxXField.GetComponent<Text>().text);
+        boundMinY = Convert.ToInt32(minYField.GetComponent<Text>().text);
+        boundMaxY = Convert.ToInt32(maxYField.GetComponent<Text>().text);
+
+        GetValues();
     }
 
+
     public string[] GetEquationParameters() {
-        return new string[5] {equationString, boundMinX, boundMaxX, boundMinY, boundMaxY};
+        return new string[5] {equationString, Convert.ToString(boundMinX), Convert.ToString(boundMaxX), Convert.ToString(boundMinY), Convert.ToString(boundMaxY)};
+    }
+
+    public void GetValues()
+    {
+        
+        double stepX = (boundMaxX - boundMinX) / totalPoints;
+        double stepY = (boundMaxY - boundMinY) / totalPoints;
+
+        string equation;
+
+
+        MathParser mathParser = new MathParser();
+
+        for (int i = 0; i < totalPoints; i++)
+        {
+            for (int j = 0; j < totalPoints; j++)
+            {
+                equation = equationString.Replace("x", Convert.ToString(boundMinX + i * stepX));
+                equation = equation.Replace("y", Convert.ToString(boundMinY + j * stepY));
+                equation = equation.Replace("+-", "-");
+                equation = equation.Replace("-+", "-");
+
+                Debug.Log(equation + " " + (boundMinX + i * stepX) + " " + boundMinX);
+                answer[i, j, 0] = boundMinX + i * stepX;
+                answer[i, j, 1] = boundMinY + i * stepY;
+                answer[i, j, 2] = mathParser.Parse(equation, true);
+            }
+        }
+
+        /*for (int i = 0; i < totalPoints; i++)
+        {
+            for (int j = 0; j< totalPoints; j++)
+            {
+                Debug.Log(answer[i, j, 2]);
+            }
+        }*/
+
+
+        
+
     }
 
 }
